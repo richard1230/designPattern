b8e7cca1f9b2148711e870744538138d1f00c96b
richard1230 <yongjinhu1230@gmail.com>
2020-05-16 13:05:02 +0800

    builderPattern

---
+++ src/main/java/createBuildPattern/builderPattern/Transmission.java
@@ -0,0 +1,9 @@
+package createBuildPattern.builderPattern;
+
+/**
+ * Just another feature of a car.
+ */
+public enum Transmission {
+    SINGLE_SPEED, MANUAL, AUTOMATIC, SEMI_AUTOMATIC
+}
+
---
+++ src/main/java/createBuildPattern/builderPattern/Type.java
@@ -0,0 +1,5 @@
+package createBuildPattern.builderPattern;
+
+public enum Type {
+    CITY_CAR, SPORTS_CAR, SUV
+}
