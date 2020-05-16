606d95ab567b81538c523a78c485388366bad5dd
richard1230 <yongjinhu1230@gmail.com>
2020-05-16 13:57:48 +0800

    checkstyle: @return param

--- src/main/java/createBuildPattern/builderPattern02/Director.java
+++ src/main/java/createBuildPattern/builderPattern02/Director.java
@@ -5,6 +5,7 @@ public class Director {
     private BanLiPaiGuSoupBuilder banLiPaiGuSoupBuilder = new BanLiPaiGuSoupBuilder();
     /**
      * 熬制冬瓜排骨汤
+     * @return DongGuaPaiGuSoup
      */
     public DongGuaPaiGuSoup buildDongGuaPaiGuSoup() {
         dongGuaPaiGuSoupBuilder.buildSoup();
@@ -13,6 +14,7 @@ public class Director {
 
     /**
      * 熬制板栗排骨汤
+     * @return BanLiPaiGuSoup
      */
     public BanLiPaiGuSoup buildBanLiPaiGuSoup() {
         banLiPaiGuSoupBuilder.buildSoup();
