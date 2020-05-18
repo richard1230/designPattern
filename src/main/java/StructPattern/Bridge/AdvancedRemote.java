1189b6f66c4944db9fbbfc1a31ca601f30f577a7
richard1230 <yongjinhu1230@gmail.com>
2020-05-18 12:31:18 +0800

    bridge

---
+++ src/main/java/StructPattern/Bridge/AdvancedRemote.java
@@ -0,0 +1,14 @@
+package StructPattern.Bridge;
+
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
---
+++ src/main/java/StructPattern/Bridge/Remote.java
@@ -0,0 +1,15 @@
+package StructPattern.Bridge;
+
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
+
