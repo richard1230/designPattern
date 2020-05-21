53e74757a9e50c079fbf5777fb1388b55b686b5b
richard1230 <yongjinhu1230@gmail.com>
2020-05-21 08:58:37 +0800

    facade

---
+++ src/main/java/StructPattern/Facade/Codec.java
@@ -0,0 +1,4 @@
+package StructPattern.Facade;
+
+public interface Codec {
+}
---
+++ src/main/java/StructPattern/Facade/OggCompressionCodec.java
@@ -0,0 +1,5 @@
+package StructPattern.Facade;
+
+public class OggCompressionCodec implements Codec {
+    public String type = "ogg";
+}
