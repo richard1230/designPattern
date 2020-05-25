76bdd91c35b9d9b30d6244aa694c69075489529e
richard1230 <yongjinhu1230@gmail.com>
2020-05-25 16:00:30 +0800

    commandpattern

---
+++ src/main/java/BehaviorPattern/CommandPattern/CopyCommand.java
@@ -0,0 +1,14 @@
+package BehaviorPattern.CommandPattern;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
---
+++ src/main/java/BehaviorPattern/CommandPattern/Demo.java
@@ -0,0 +1,8 @@
+package BehaviorPattern.CommandPattern;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
