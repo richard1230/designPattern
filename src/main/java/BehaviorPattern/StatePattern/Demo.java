e56d069c5c3ffffc5da3cbc95eea03a4a4572f4d
richard1230 <yongjinhu1230@gmail.com>
2020-06-02 14:09:38 +0800

    statePattern

---
+++ src/main/java/BehaviorPattern/StatePattern/Demo.java
@@ -0,0 +1,13 @@
+package BehaviorPattern.StatePattern;
+
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
